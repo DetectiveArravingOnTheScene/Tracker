ebeba14c986a45d9aeb9dcf5c259a8c6cafcad7b
DetectiveArrivingOnTheScene <yadevichr@gmail.com>
2026-04-11 15:18:52 +0300

    fix: update the card

--- tracker_app/lib/presentation/widgets/app_card.dart
+++ tracker_app/lib/presentation/widgets/app_card.dart
@@ -10,14 +10,28 @@ class AppCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.surface,
-        border: BoxBorder.fromLTRB(
-          top: BorderSide(color: AppColors.white),
-          left: BorderSide(color: AppColors.hintColor),
-          right: BorderSide(color: AppColors.hintColor),
+        borderRadius: BorderRadius.circular(16),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFFFFFFFF), // Top/Left (shine)
+            Color(0xFFCBD5E1), // Center/Right (medium-light)
+            Color(0xFF475569), // Bottom/Right (shadow)
+          ],
+          stops: [0.0, 0.5, 1.0],
         ),
       ),
-      child: child,
+      padding: const EdgeInsets.all(1), // Acts as the border width
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.surface, // Masks the center of the gradient
+          // Subtract the padding (4) from the outer radius (16)
+          // for a perfectly concentric curve!
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: child,
+      ),
     );
   }
 }
